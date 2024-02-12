42e6c795746b843971379d37a8704d3d59ffb931
Цырульников Кирилл Алексеевич <kacyrulnikov@dev.vtb.ru>
2024-02-12 07:21:13 +0300

    Ballistic v.1.0.1

--- 10. Служебные программы/1. Программа баллистики/lib/integration_method.py
+++ 10. Служебные программы/1. Программа баллистики/lib/integration_method.py
@@ -9,49 +9,78 @@ def runge_kutta(arr_parameters, t, dt):
     x_old = arr_parameters[2]
     y_old = arr_parameters[3]
     m_old = arr_parameters[4]
+    omega_z_old = arr_parameters[5]
+    pitch_old = arr_parameters[6]
 
-    k_v_1 = dt * mm.dv_dt(v_old, y_old, tetta_c_old, m_old, t)
-    k_tetta_c_1 = dt * mm.dtetta_c_dt(v_old, y_old, tetta_c_old, m_old)
+    if y_old < ip.HEIGHT_GUIDE:
+        omega_z_old = ip.OMEGA_Z_0
+        pitch_old = ip.PITCH_0
+    # ------------------------------------------------------------------- #
+
+    alpha = mm.do_alpha(pitch_old, tetta_c_old)
+
+    k_v_1 = dt * mm.dv_dt(v_old, y_old, tetta_c_old, alpha, m_old, t)
+    k_tetta_c_1 = dt * mm.dtetta_c_dt(v_old, y_old, tetta_c_old, alpha, m_old, t)
     k_x_1 = dt * mm.dx_dt(v_old, tetta_c_old)
     k_y_1 = dt * mm.dy_dt(v_old, tetta_c_old)
     k_m_1 = dt * mm.dm_dt(t)
+    k_omega_z_1 = dt * mm.domega_z_dt(v_old, y_old, alpha)
+    k_pitch_1 = dt * mm.dpitch_dt(omega_z_old)
+    # ------------------------------------------------------------------- #
 
-    k_v_2 = dt * mm.dv_dt(v_old + k_v_1 / 2, y_old + k_y_1 / 2, tetta_c_old + k_tetta_c_1 / 2, m_old + k_m_1 / 2,
+    alpha = mm.do_alpha(pitch_old + k_pitch_1 / 2, tetta_c_old + k_tetta_c_1 / 2)
+
+    k_v_2 = dt * mm.dv_dt(v_old + k_v_1 / 2, y_old + k_y_1 / 2, tetta_c_old + k_tetta_c_1 / 2, alpha, m_old + k_m_1 / 2,
                           t + dt / 2)
-    k_tetta_c_2 = dt * mm.dtetta_c_dt(v_old + k_v_1 / 2, y_old + k_y_1 / 2, tetta_c_old + k_tetta_c_1 / 2,
-                                      m_old + k_m_1 / 2)
+    k_tetta_c_2 = dt * mm.dtetta_c_dt(v_old + k_v_1 / 2, y_old + k_y_1 / 2, tetta_c_old + k_tetta_c_1 / 2, alpha,
+                                      m_old + k_m_1 / 2, t + dt / 2)
     k_x_2 = dt * mm.dx_dt(v_old + k_v_1 / 2, tetta_c_old + k_tetta_c_1 / 2)
     k_y_2 = dt * mm.dy_dt(v_old + k_v_1 / 2, tetta_c_old + k_tetta_c_1 / 2)
     k_m_2 = dt * mm.dm_dt(t)
+    k_omega_z_2 = dt * mm.domega_z_dt(v_old + k_v_1 / 2, y_old + k_y_1 / 2, alpha)
+    k_pitch_2 = dt * mm.dpitch_dt(omega_z_old + k_omega_z_1 / 2)
+    # ------------------------------------------------------------------- #
 
-    k_v_3 = dt * mm.dv_dt(v_old + k_v_2 / 2, y_old + k_y_2 / 2, tetta_c_old + k_tetta_c_2 / 2, m_old + k_m_2 / 2,
+    alpha = mm.do_alpha(pitch_old + k_pitch_2 / 2, tetta_c_old + k_tetta_c_2 / 2)
+
+    k_v_3 = dt * mm.dv_dt(v_old + k_v_2 / 2, y_old + k_y_2 / 2, tetta_c_old + k_tetta_c_2 / 2, alpha, m_old + k_m_2 / 2,
                           t + dt / 2)
-    k_tetta_c_3 = dt * mm.dtetta_c_dt(v_old + k_v_2 / 2, y_old + k_y_2 / 2, tetta_c_old + k_tetta_c_2 / 2,
-                                      m_old + k_m_2 / 2)
+    k_tetta_c_3 = dt * mm.dtetta_c_dt(v_old + k_v_2 / 2, y_old + k_y_2 / 2, tetta_c_old + k_tetta_c_2 / 2, alpha,
+                                      m_old + k_m_2 / 2, t + dt / 2)
     k_x_3 = dt * mm.dx_dt(v_old + k_v_2 / 2, tetta_c_old + k_tetta_c_2 / 2)
     k_y_3 = dt * mm.dy_dt(v_old + k_v_2 / 2, tetta_c_old + k_tetta_c_2 / 2)
     k_m_3 = dt * mm.dm_dt(t)
+    k_omega_z_3 = dt * mm.domega_z_dt(v_old + k_v_2 / 2, y_old + k_y_2 / 2, alpha)
+    k_pitch_3 = dt * mm.dpitch_dt(omega_z_old + k_omega_z_2 / 2)
+    # ------------------------------------------------------------------- #
 
-    k_v_4 = dt * mm.dv_dt(v_old + k_v_3, y_old + k_y_3, tetta_c_old + k_tetta_c_3, m_old + k_m_3, t + dt)
-    k_tetta_c_4 = dt * mm.dtetta_c_dt(v_old + k_v_3, y_old + k_y_3, tetta_c_old + k_tetta_c_3, m_old + k_m_3)
+    alpha = mm.do_alpha(pitch_old + k_pitch_3, tetta_c_old + k_tetta_c_3)
+
+    k_v_4 = dt * mm.dv_dt(v_old + k_v_3, y_old + k_y_3, tetta_c_old + k_tetta_c_3, alpha, m_old + k_m_3, t + dt)
+    k_tetta_c_4 = dt * mm.dtetta_c_dt(v_old + k_v_3, y_old + k_y_3, tetta_c_old + k_tetta_c_3, alpha, m_old + k_m_3, t + dt)
     k_x_4 = dt * mm.dx_dt(v_old + k_v_3, tetta_c_old + k_tetta_c_3)
     k_y_4 = dt * mm.dy_dt(v_old + k_v_3, tetta_c_old + k_tetta_c_3)
     k_m_4 = dt * mm.dm_dt(t)
+    k_omega_z_4 = dt * mm.domega_z_dt(v_old + k_v_3, y_old + k_y_3, alpha)
+    k_pitch_4 = dt * mm.dpitch_dt(omega_z_old + k_omega_z_3)
+    # ------------------------------------------------------------------- #
 
     v_new = v_old + (k_v_1 + 2 * k_v_2 + 2 * k_v_3 + k_v_4) / 6
     tetta_c_new = tetta_c_old + (k_tetta_c_1 + 2 * k_tetta_c_2 + 2 * k_tetta_c_3 + k_tetta_c_4) / 6
     x_new = x_old + (k_x_1 + 2 * k_x_2 + 2 * k_x_3 + k_x_4) / 6
     y_new = y_old + (k_y_1 + 2 * k_y_2 + 2 * k_y_3 + k_y_4) / 6
     m_new = m_old + (k_m_1 + 2 * k_m_2 + 2 * k_m_3 + k_m_4) / 6
+    omega_z_new = omega_z_old + (k_omega_z_1 + 2 * k_omega_z_2 + 2 * k_omega_z_3 + k_omega_z_4)
+    pitch_new = pitch_old + (k_pitch_1 + 2 * k_pitch_2 + 2 * k_pitch_3 + k_pitch_4)
 
-    return [v_new, tetta_c_new, x_new, y_new, m_new]
+    return [v_new, tetta_c_new, x_new, y_new, m_new, omega_z_new, pitch_new]
 
 
 def calculate():
-    print('%-5s %-8s %-7s %-8s %-7s %-7s' % ('t, c', 'V, m/c', 'θ, °',
-                                             'x, m', 'y, m', 'm, kg'))
+    print('%-5s %-8s %-7s %-8s %-7s %-7s %-7s %-7s %-7s' % ('t, c', 'V, m/c', 'θ, °',
+                                             'x, m', 'y, m', 'm, kg', 'omega', 'pitch', 'alpha'))
 
-    parameters = [ip.V_0, ip.TETTA_C_0, ip.X_0, ip.Y_0, ip.MASS_0]
+    parameters = [ip.V_0, ip.TETTA_C_0, ip.X_0, ip.Y_0, ip.MASS_0, ip.OMEGA_Z_0, ip.PITCH_0]
     dt = ip.DELTA_T
     t = ip.T_0
     t_prev = t
@@ -59,9 +88,9 @@ def calculate():
     flg_guide = False
 
     while True:
+        alpha = mm.do_alpha(parameters[6], parameters[1])
         # TODO добавить  четко точку схода с направляющей
-        print('%-5.1f %-8.3f %-7.2f %-8.2f %-7.2f %-7.2f' % (t, parameters[0], math.degrees(parameters[1]),
-                                                             parameters[2], parameters[3], parameters[4]))
+        print(f'{t:<5.1f} {parameters[0]:<8.3f} {math.degrees(parameters[1]):<7.2f} {parameters[2]:<8.2f} {parameters[3]:<7.2f} {parameters[4]:<7.2f} {parameters[5]:<7.2f} {math.degrees(parameters[6]):<7.2f} {alpha:<7.2f}')
 
         parameters_prev = parameters
         parameters = runge_kutta(parameters, t, dt)
--- 10. Служебные программы/1. Программа баллистики/lib/mathematical_model.py
+++ 10. Служебные программы/1. Программа баллистики/lib/mathematical_model.py
@@ -21,17 +21,38 @@ def force_y_a(velocity, height):
     return velocity ** 2 * dencity * ip.S_M * ip.C_Y_A / 2
 
 
-def dv_dt(velocity, height, tetta_c, m, t):
+def momentum_z(velocity, height, alpha):
+    dencity = atm.dencity(height)
+    return velocity ** 2 * dencity * ip.S_M * ip.LENGHT * ip.M_Z_KOEF * alpha
+
+
+def do_alpha(pitch, tetta_c):
+    # return 0.1
+    return pitch - tetta_c
+
+
+def dv_dt(velocity, height, tetta_c, alpha, m, t):
     x_a = force_x_a(velocity, height)
-    return (do_thrust(t) - x_a) / m - atm.acceleration_g(height) * math.sin(tetta_c)
+    return (do_thrust(t) * math.cos(alpha) - x_a) / m - atm.acceleration_g(height) * math.sin(tetta_c)
 
 
-def dtetta_c_dt(velocity, height, tetta_c, m):
+def dtetta_c_dt(velocity, height, tetta_c, alpha, m, t):
     if height <= ip.HEIGHT_GUIDE * math.sin(ip.TETTA_C_0):
         return 0
     else:
         y_a = force_y_a(velocity, height)
-        return y_a / (m * velocity) + atm.acceleration_g(height) * math.cos(tetta_c) / velocity
+        return (y_a + do_thrust(t) * math.sin(alpha)) / (m * velocity) + atm.acceleration_g(
+            height) * math.cos(tetta_c) / velocity
+    # TODO по результатам работы проги тетта мало меняется от 90 градусов - подумать, почему (ракета не ложится
+    #  набок, хотя из-за вращетального движения должна
+
+
+def domega_z_dt(velocity, height, alpha):
+    return momentum_z(velocity, height, alpha) / ip.INERTION_MOMENTUM
+
+
+def dpitch_dt(omega_z):
+    return omega_z
 
 
 def dx_dt(velocity, tetta_c):
@@ -48,5 +69,3 @@ def dm_dt(t):
         return ip.DM_DT
     else:
         return 0
-
-# TODO добавить вращение вокруг ЦМ
